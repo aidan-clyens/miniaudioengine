548d4b7e8830943e11b543e4b0a0c7dae66fd1c6
Aidan Clyens <aidanc@rogers.com>
2026-04-11 14:04:43 -0400

    Add missing includes

--- src/data/audio/include/audiocallbackhandler.h
+++ src/data/audio/include/audiocallbackhandler.h
@@ -1,12 +1,15 @@
 #ifndef __AUDIO_CALLBACK_HANDLER_H__
 #define __AUDIO_CALLBACK_HANDLER_H__
 
+#include <memory>
+#include <vector>
+
 #include <rtaudio/RtAudio.h>
+
+#include "audiodataplane.h"
 
 namespace miniaudioengine::core
 {
-// Forward declaration
-typedef std::shared_ptr<class AudioDataPlane> AudioDataPlanePtr;
 
 struct AudioCallbackContext
 {
